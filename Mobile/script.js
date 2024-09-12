4f6d9cf11c140bebb05e3d46a01fea6d35032569
gabeswk <147556284+gabeswk@users.noreply.github.com>
2024-09-12 10:17:36 -0300

    Add files via upload

--- Mobile/script.js
+++ Mobile/script.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 document.getElementById('Menu-toggle').addEventListener('click', function(){
     var menuList = document.getElementById('menu-list');
     menuList.classList.toggle('active');
-=======
-document.getElementById('Menu-toggle').addEventListener('click', function(){
-    var menuList = document.getElementById('menu-list');
-    menuList.classList.toggle('active');
->>>>>>> 64272902bf1e28a4f7a520b274771d0143f967f3
-});
+}
+);
